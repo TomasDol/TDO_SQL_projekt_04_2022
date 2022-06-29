2037502b98fb3c89f150f4d1f87632d731712248
TomasDol <104095319+TomasDol@users.noreply.github.com>
2022-06-29 19:35:58 +0200

    Update case_3.sql

--- case_3.sql
+++ case_3.sql
@@ -7,14 +7,13 @@ SELECT
 FROM (SELECT 
 		YEAR (date_from) AS rok ,		
 		name ,
-		value as cena2018 ,
+		value AS cena2018 ,
 		MIN(value) OVER (PARTITION BY category_code) AS min2006a2018 ,
 		MAX(value) OVER (PARTITION BY category_code) AS max2006a2018
 	FROM czechia_price cp 
 	LEFT JOIN czechia_price_category cpc 
 		ON cp.category_code = cpc.code 
-	WHERE 1 = 1
-		AND YEAR (date_from) = 2006 
+	WHERE YEAR (date_from) = 2006 
 		OR YEAR (date_from) = 2018
 	GROUP BY category_code, rok) vstup
 WHERE rok = 2018
