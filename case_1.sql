a8ab73ffee110362f6fd5015ac144cad3119c12c
TomasDol <104095319+TomasDol@users.noreply.github.com>
2022-06-29 19:36:46 +0200

    Update case_1.sql

--- case_1.sql
+++ case_1.sql
@@ -2,11 +2,6 @@
  * Projekt SQL - otázka 1 - Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
  */ 
 
-#Krok 1 - doplnění ročních průměrů
-#Krok 2 - filtrace
-#Krok 3 - doplnění názvů z payroll_Ind_branch přes left join + propojení jednotek z cz_payroll_unit
-#Krok 4 - filtrace
-#Krok 5 - export a vytvoření tabulky 
 
 -- CREATE OR REPLACE TABLE T_TDO_Projekt_SQL_Case1_data AS
 	SELECT 
@@ -19,14 +14,13 @@
 			*,
 			AVG(value) OVER (PARTITION BY payroll_year, industry_branch_code, calculation_code, unit_code) as Y_vse_prumer
 		FROM czechia_payroll) x
-	LEFT JOIN czechia_payroll_industry_branch cpib 			#krok 3 - propojení s druhou tabulkou
+	LEFT JOIN czechia_payroll_industry_branch cpib 			
 		ON x.industry_branch_code = cpib.code 
 	LEFT JOIN czechia_payroll_unit cpu 
 		ON x.unit_code = cpu.code 
-	WHERE 1=1
-		AND x.payroll_quarter = 4							#krok 2 - filtrace jen jeden řádek za rok a to 4Q
-		AND x.calculation_code = 100  						#krok 2 - filtrace jen fyzický dle čísleníku payroll_calculation
-		AND x.unit_code = 200 								#krok 4 - filtrace jen Kč
+	WHERE x.payroll_quarter = 4							
+		AND x.calculation_code = 100  						
+		AND x.unit_code = 200 								
 	ORDER BY 
 		x.industry_branch_code ,
 		x.payroll_year 
