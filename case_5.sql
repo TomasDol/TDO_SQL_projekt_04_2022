bf60cd8351db5c05a547450e9d8db7c1c57865e6
TomasDol <104095319+TomasDol@users.noreply.github.com>
2022-07-20 20:28:04 +0200

    Update case_5.sql
    Spojení na 3 výstupy

--- case_5.sql
+++ case_5.sql
@@ -3,12 +3,40 @@
  */ 
 
 SELECT 
-	`year` ,
-	GDP 
-FROM economies e 
-WHERE 1=1
-	AND e.country = 'Czech Republic'
-	AND `year` <2019
-	AND `year` >2005
-
-#+ soubor z case 4
+	*
+FROM (
+	SELECT 
+		`year` ,
+		GDP 
+	FROM economies e 
+	WHERE 1=1
+		AND e.country = 'Czech Republic'
+		AND `year` <2019
+		AND `year` >2005
+		) X
+LEFT JOIN (	
+	SELECT 
+		vystup1.rok ,
+		vystup1.rocni_cena
+	FROM (
+		SELECT 
+			*,
+			YEAR (date_from) AS rok ,
+			AVG(value) OVER (PARTITION BY YEAR(date_from)) AS rocni_cena
+		FROM czechia_price cp 
+		LEFT JOIN czechia_price_category cpc 
+			ON cp.category_code = cpc.code 
+		GROUP BY YEAR (date_from), name	)vystup1
+	WHERE category_code = 116103						
+	)Y
+	ON X.`year` = Y.rok 
+LEFT JOIN (
+	SELECT
+		*
+	FROM T_TDO_Projekt_SQL_out1_mzda ttpsom 
+	WHERE 1 = 1
+		AND rok <2019
+		AND rok >2005
+	GROUP BY rok 
+	) Z
+	ON X.`year` = Z.rok 
